ba341bc980f25fb59d682b4ccd2303f1645ebc32
Joerassic <johanne.cornelius@mac.com>
2025-01-14 11:41:24 +0100

    gamma=0.002 instead of gamma=0.001

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     data = digits.images.reshape((n_samples, -1))
 
     # Create a classifier: a support vector classifier
-    clf = svm.SVC(gamma=0.001)
+    clf = svm.SVC(gamma=0.002)
 
     # Split data into 50% train and 50% test subsets
     X_train, X_test, y_train, y_test = train_test_split(data, digits.target, test_size=0.5, shuffle=False)
